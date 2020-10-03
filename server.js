0e27d816673fb54668acdaa293b5dcb7e0464c54
ajnielsen727 <46825549+ajnielsen727@users.noreply.github.com>
2020-10-03 17:28:27 -0400

    Initial server side commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
