62dc57a0b877d76624a0e100d43d17770ac22662
Dean E. Peterson <dean.e.penguin@gmail.com>
2020-10-05 14:38:36 -0400

    Separate staff create user api from register user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -73,5 +73,64 @@ router.post(
   }
 );
 
+// @route   POST api/auth/register
+// @desc    Register user
+// @access  Public
+router.post(
+  '/register', 
+  [
+    check('firstName', 'First name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password at least 8 characters long').isLength({ min: 8 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Make sure user doesn't already exist.
+      let user = await User.findOne( { email: req.body.email });
+      if (user) {
+        return res.status(400).json({ errors: [ { msg: 'User already exists' }] });
+      }
+    
+      // Hash password.
+      user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.password, salt);
+
+      // Save user.
+      await user.save();
+
+      // Return jsonwebtoken.
+      const payload = {
+        user: {
+          id: user.id, // ID from saving user to database.
+          isStaff: user.isStaff,          
+        }
+      }
+
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: config.get('jwtExpires') },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
