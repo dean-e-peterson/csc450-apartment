72f4d782215664aa29805d3841d352c3bed55615
ajnielsen727 <46825549+ajnielsen727@users.noreply.github.com>
2020-10-07 19:58:46 -0400

    profiles

---
+++ models/StaffProfile.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
