72c669ad07d458eb97f55c8eb3ef3c1daa2946f4
Dean E. Peterson <dean.e.penguin@gmail.com>
2020-10-14 20:04:22 -0400

    Initial material cleanups to compile

--- client/src/homepage.js
+++ client/src/homepage.js
@@ -1,19 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import LocationFilteredList from "./LocationFilteredList";
-import * as api from "../../service/api";
-import { GridContainer, GridItem } from "canvas-ui-react";
-import ReprocessTrans from "./ReprocessTrans";
-import { bool } from "prop-types";
-import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import {
   AppBar,
   Toolbar,
@@ -22,18 +9,14 @@ import {
   Typography,
   Button
 } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import ReactDOM from "react-dom";
-import LocationPageStyle from "./LocationPageStyle.css";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-
-import { Grid } from "canvas-ui-react";
-import { Apartment } from "@material-ui/icons";
+import Grid from "@material-ui/core/Grid";
+import GridList from '@material-ui/core/GridList';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -107,7 +90,7 @@ export default function ButtonAppBar() {
           </CardActions>
         </Card>
       </Grid.Item>
-      <GridItem>
+      <GridList>
         <Card className={classes.root}>
           <CardActionArea>
             <CardMedia
@@ -144,7 +127,7 @@ export default function ButtonAppBar() {
             </Button>
           </CardActions>
         </Card>
-      </GridItem>
+      </GridList>
     </Grid.Container>
   );
   {
