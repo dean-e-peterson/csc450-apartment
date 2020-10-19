5458a5bb6e70187bd5776a9efd5a2a946fdeba87
Dean E. Peterson <dean.e.penguin@gmail.com>
2020-10-19 12:39:58 -0400

    Improve post layout

--- client/src/layout/Post.js
+++ client/src/layout/Post.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import {
   Button,
   Card,
   CardActions,
   CardContent,
   Collapse,
+  Grid,
   Typography,
 } from "@material-ui/core"
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -20,8 +21,17 @@ export default function Post({ post, authUser }) {
     <Card key={post._id}>
       <CardContent>
         <Typography component="div">
-          <p>{post.name} at {post.date}</p>
-          <p>{post.text}</p>
+          <Grid container>
+            <Grid item xs={6}>
+              <p><strong>{post.name}</strong></p>
+            </Grid> 
+            <Grid item xs={6}>
+              <p>at {(new Date(post.date)).toLocaleString("en-US", { dateStyle: "full", timeStyle: "short" })}</p>
+            </Grid>
+            <Grid item xs={12}>
+              <p>{post.text}</p>
+            </Grid>
+          </Grid>
         </Typography>
       </CardContent>
       <CardActions>
@@ -32,11 +42,25 @@ export default function Post({ post, authUser }) {
       </CardActions>
       <Collapse in={expanded} mountOnEnter unmountOnExit>
         <CardContent>
-          {
-            post.comments.map(comment =>
-              comment.text
-            )
-          }
+          <Typography>
+            <Grid container>
+              {
+                post.comments.map(comment =>
+                  <Fragment>
+                    <Grid item xs={6}>
+                      <p><strong>{comment.name}</strong></p>
+                    </Grid> 
+                    <Grid item xs={6}>
+                      <p>at {(new Date(comment.date)).toLocaleString("en-US", { dateStyle: "full", timeStyle: "short" })}</p>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <p>{comment.text}</p>
+                    </Grid>
+                  </Fragment>
+                )
+              }
+            </Grid>
+          </Typography>
         </CardContent>
       </Collapse>
     </Card>
