8abb0035b3c2a390df2f0c9c11429262db0b9efd
Dean E. Peterson <dean.e.penguin@gmail.com>
2020-11-06 15:41:28 -0500

    Add comment routes to maint request server side

--- routes/api/requests.js
+++ routes/api/requests.js
@@ -184,4 +184,127 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   POST api/requests/comment/:id
+// @desc    Comment on a request
+// @access  Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required').not().isEmpty(),
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const request = await Request.findById(req.params.id);
+
+      // Check that user is staff or is commenting on their own unit's request.
+      if (! (req.user.isStaff || user.unit.toString() === request.unit.toString())) {
+        return res.status(403).json({ errors: [{ msg: 'Not authorized'}] });
+      }      
+
+      const newComment = {
+        text: req.body.text,
+        name: user.firstName + ' ' + user.lastName,
+        user: req.user.id
+      };
+
+      request.comments.push(newComment);
+
+      await request.save();
+
+      res.json(request.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+// @route   PATCH api/requests/comment/:id/:comment_id
+// @desc    Modify a comment on a request
+// @access  Private
+router.patch(
+  '/comment/:id/:comment_id',
+  auth,
+  [
+    check('text', 'Text is required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Find request.
+      const request = await Request.findById(req.params.id);
+
+      // Pull out comment.
+      const comment = request.comments.find(comment => comment.id === req.params.comment_id);
+
+      // Make sure comment exists.
+      if (!comment) {
+        return res.status(404).json({ msg: 'Comment does not exist' });
+      }
+
+      // Only allow a user to modify their own comment unless they are staff.
+      if (! (req.user.isStaff || comment.user.toString() === req.user.id)) {
+        return res.status(403).json({ errors: [{ msg: 'Not authorized'}] });
+      }
+      
+      // Modify comment.
+      comment.text = req.body.text;
+
+      // Save whole request.
+      await request.save();
+
+      // Return all comments.
+      res.json(request.comments);      
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  } 
+);
+
+// @route   DELETE api/requests/comment/:id/:comment_id
+// @desc    Delete a comment from a request
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    // Find request.
+    const request = await Request.findById(req.params.id);
+
+    // Pull out comment.
+    const comment = request.comments.find(comment => comment.id === req.params.comment_id);
+
+    // Make sure comment exists.
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Only allow a user to delete their own comment unless they are staff.
+    if (! (req.user.isStaff || comment.user.toString() === req.user.id)) {
+      return res.status(403).json({ errors: [{ msg: 'Not authorized'}] });
+    }
+
+    request.comments = request.comments.filter( ({ id }) => id !== req.params.comment_id );
+
+    await request.save();
+
+    res.json(request.comments);    
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
